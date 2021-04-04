b9c2d9e695c00de2ca46b570125d1145bdbd6f73
Ignacio Quinteros <i.quinteros@hotmail.com>
2021-04-04 00:03:10 -0400

    HomeView added

---
+++ lib/views/home_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('EcoApp'),
+      ),
+      body: Center(
+        child: Text('HomeView'),
+      ),
+    );
+  }
+}
