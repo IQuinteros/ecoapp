990394706ac0444c5274de538257d8dddf17d3b4
IQuinteros <i.quinteros@hotmail.com>
2021-05-24 16:25:31 -0400

    Fixing items size with big width screen

--- lib/views/widgets/home/featured_product.dart
+++ lib/views/widgets/home/featured_product.dart
@@ -106,7 +106,7 @@ class _FeaturedProductState extends State<FeaturedProduct> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width - 80),
+              constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.8),
               child: card,
             ),
           ],
