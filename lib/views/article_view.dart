c755eaa250d1a307e99e468edeabab0976565f90
IQuinteros <i.quinteros@hotmail.com>
2021-05-27 12:04:05 -0400

    Added questions section to article view

--- lib/views/article_view.dart
+++ lib/views/article_view.dart
@@ -223,7 +223,9 @@ class ArticleView extends StatelessWidget {
       Divider(thickness: 1,),
       getEcoDetail(context, article),
       Divider(thickness: 1,),
-      getStoreDescription(context, article)
+      getStoreDescription(context, article),
+      Divider(thickness: 1,),
+      getQuestions(context, article)
     ];
   }
 
@@ -410,6 +412,119 @@ class ArticleView extends StatelessWidget {
           rowContent,
           SizedBox(height: 20.0),
           NormalButton(text: 'Ver más datos del vendedor', onPressed: (){})
+        ],
+      ),
+    );
+  }
+
+  getQuestions(BuildContext context, ArticleModel article) {
+    final searchField = Container(
+      margin: EdgeInsets.symmetric(
+        vertical: 15.0
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.25),
+            blurRadius: 5
+          )
+        ]
+      ),
+      child: TextField(
+        style: GoogleFonts.montserrat(),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0)
+          ),
+          hintText: 'Pregunta al vendedor',
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: 20.0,
+            vertical: 5.0
+          ),
+          hintStyle: GoogleFonts.montserrat(
+            fontSize: 14
+          )
+        ),
+        readOnly: false,
+        onTap: (){
+          // TODO: Open dialog
+        },
+      ),
+    );
+
+    final question = Container(
+      margin: EdgeInsets.symmetric(
+        vertical: 10.0
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Qué materiales tiene exactamente?',
+                style: GoogleFonts.montserrat(),
+              ),
+              Text(
+                '${DateTime.now().day.toString()}/${DateTime.now().month.toString()}/${DateTime.now().year.toString()}',
+                style: GoogleFonts.montserrat(
+                  color: Colors.black45
+                ),
+              )
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.only(
+              top: 10.0,
+              left: 20.0
+            ),
+            child: Text(
+              'Contiene elementos extremadamente amigables al ecosistema como este, este y este otro, Saludos!',
+              style: GoogleFonts.montserrat(
+                fontWeight: FontWeight.w300
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+
+    final questions = Column(
+      children: [
+        question,
+        question
+      ],
+    );
+
+    return Container(
+      margin: EdgeInsets.symmetric(
+        vertical: 20.0,
+        horizontal: 20.0
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Preguntas y respuestas',
+            style: GoogleFonts.montserrat(
+              fontWeight: FontWeight.w500,
+              fontSize: 16
+            ),
+          ),
+          searchField,
+          Text(
+            'Últimas preguntas',
+            style: GoogleFonts.montserrat(
+              fontWeight: FontWeight.w500,
+              fontSize: 15
+            ),
+          ),
+          SizedBox(height: 5.0),
+          questions,
+          SizedBox(height: 15.0),
+          NormalButton(text: 'Ver más preguntas', onPressed: (){})
         ],
       ),
     );
