c59cf31c3c3d7756209e8604552d9703b34249c8
IQuinteros <i.quinteros@hotmail.com>
2021-05-27 14:38:39 -0400

    Adding cart model

---
+++ lib/models/cart.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecoapp/models/article.dart';
+import 'package:flutter_ecoapp/models/base.dart';
+
+class CartModel extends BaseModel
+{
+  List<ArticleModel> articles;
+
+  CartModel({
+    @required int id,
+    this.articles = const [],
+  }) : super(id: id);
+
+  double get totalPrice {
+    double total = 0;
+    articles.forEach((element) { total += element.price; });
+    return total;
+  } 
+
+}
