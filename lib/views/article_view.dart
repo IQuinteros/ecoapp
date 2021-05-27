9202603507f66a5ce6b67942b92f324e00460ee5
IQuinteros <i.quinteros@hotmail.com>
2021-05-27 12:19:25 -0400

    Photo section added

--- lib/views/article_view.dart
+++ lib/views/article_view.dart
@@ -205,6 +205,7 @@ class ArticleView extends StatelessWidget {
     );
 
     return [
+      getPhotosSection(context, article),
       title,
       SizedBox(height: 5.0),
       rating,
@@ -526,6 +527,56 @@ class ArticleView extends StatelessWidget {
           SizedBox(height: 15.0),
           NormalButton(text: 'Ver más preguntas', onPressed: (){})
         ],
+      ),
+    );
+  }
+
+  getPhotosSection(BuildContext context, ArticleModel article) {
+    final photo = GestureDetector(
+      child: Container(
+        margin: EdgeInsets.only(
+          right: 10.0
+        ),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.25),
+              blurRadius: 5,
+              spreadRadius: 1
+            )
+          ]
+        ),
+        child: Image(
+          image: NetworkImage(article.photos[0].photoUrl),
+          width: 150,
+          fit: BoxFit.cover,
+        ),
+      ),
+      onTap: (){},
+    );
+
+    return Container(
+      height: 100,
+      margin: EdgeInsets.only(
+        bottom: 10.0
+      ),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Container(
+          constraints: BoxConstraints(
+            minWidth: MediaQuery.of(context).size.width
+          ),
+          margin: EdgeInsets.symmetric(
+            horizontal: 20.0
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              photo,
+              photo,
+            ],
+          ),
+        ),
       ),
     );
   }
