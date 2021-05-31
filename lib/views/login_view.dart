a9812c870be6b46e7e659f21c8d9a7a0e4bd9596
IQuinteros <i.quinteros@hotmail.com>
2021-05-31 14:05:22 -0400

    Login view added

---
+++ lib/views/login_view.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecoapp/models/store.dart';
+import 'package:flutter_ecoapp/views/debug/debug.dart';
+import 'package:flutter_ecoapp/views/style/colors.dart';
+import 'package:flutter_ecoapp/views/widgets/bottom_nav_bar.dart';
+import 'package:flutter_ecoapp/views/widgets/normal_button.dart';
+import 'package:flutter_ecoapp/views/widgets/search_bar.dart';
+import 'package:flutter_ecoapp/views/widgets/store/storeview/store_cover_section.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        centerTitle: false,
+        elevation: 0,
+        title: Text(
+          'Iniciar sesión',
+          style: GoogleFonts.montserrat(
+            fontWeight: FontWeight.w500
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.keyboard_arrow_left_rounded),
+          color: EcoAppColors.MAIN_COLOR,
+          iconSize: 40,
+          onPressed: (){
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: SafeArea(child: mainContent(context)),
+      bottomNavigationBar: EcoBottomNavigationBar(
+        currentIndex: 0,
+          onTap: (value){
+        },
+      )
+    );
+  }
+
+  Widget mainContent(BuildContext context){
+    return SingleChildScrollView(
+      child: Container(
+        margin: EdgeInsets.symmetric(
+          horizontal: 20.0,
+          vertical: 20.0
+        ),
+        child: Column(
+          children: [
+            getInput(
+              header: 'Email',
+              hint: 'Ingresa tu email',
+              icon: Icons.mail
+            ),
+            getInput(
+              header: 'Contraseña',
+              hint: 'Ingresa tu contraseña',
+              icon: Icons.vpn_key
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: 40.0
+              ),
+              child: NormalButton(
+                text: 'Iniciar sesión', 
+                onPressed: (){}
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                left: 40.0,
+                right: 40.0,
+                top: 20.0
+              ),
+              child: NormalButton(
+                text: 'Continuar con Google', 
+                onPressed: (){}
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Divider(thickness: 1,),
+            Container(
+              margin: EdgeInsets.only(
+                left: 40.0,
+                right: 40.0,
+                top: 20.0
+              ),
+              child: NormalButton(
+                text: '¡Soy nuevo! Quiero registrarme', 
+                onPressed: (){}
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getInput({String header, String hint, IconData icon}){
+    return Container(
+      margin: EdgeInsets.only(
+        bottom: 20.0
+      ),
+      child: TextField(
+        style: GoogleFonts.montserrat(),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0)
+          ),
+          hintText: hint,
+          contentPadding: EdgeInsets.symmetric(
+            horizontal: 20.0,
+            vertical: 5.0
+          ),
+          floatingLabelBehavior: FloatingLabelBehavior.auto,
+          labelText: header,
+          prefixIcon: Icon(icon)
+        ),
+      ),
+    );
+  }
+
+}
+
+
---
+++ lib/views/widgets/profile/profileview/profile_cover_section.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ProfileCoverSection extends StatelessWidget {
+  const ProfileCoverSection({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final img = Container(
+      clipBehavior: Clip.antiAlias,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.5),
+            blurRadius: 5,
+            offset: Offset(0, 1)
+          )
+        ]
+      ),
+      child: Image(
+        image: NetworkImage('https://picsum.photos/500/300'),
+        fit: BoxFit.cover,
+      ),
+      width: 80,
+      height: 80,
+    );
+
+    final content = Container(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Iniciar sesión',
+            style: GoogleFonts.montserrat(
+              fontSize: 16,
+              fontWeight: FontWeight.w500
+            ),
+            textAlign: TextAlign.start,
+          ),
+          Text(
+            'Inicia sesión fácilmente y disfruta de una experiencia completa',
+            style: GoogleFonts.montserrat(),
+            textAlign: TextAlign.start,
+          )
+        ],
+      ),
+    );
+
+    return GestureDetector(
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 20.0,
+          vertical: 20.0
+        ),
+        child: Row(
+          children: [
+            img,
+            SizedBox(width: 20.0,),
+            Expanded(child: content)
+          ],
+        ),
+      ),
+      onTap: (){
+        Navigator.pushNamed(context, 'login');
+      },
+    );
+  }
+}
