0ffc628fab8f50431eb551416b4f274833094c80
IQuinteros <i.quinteros@hotmail.com>
2021-06-09 15:57:41 -0400

    Adding questions api to article bloc

--- lib/bloc/article_bloc.dart
+++ lib/bloc/article_bloc.dart
@@ -2,14 +2,17 @@ import 'package:flutter_ecoapp/bloc/base_bloc.dart';
 import 'package:flutter_ecoapp/models/article.dart';
 import 'package:flutter_ecoapp/models/opinion.dart';
 import 'package:flutter_ecoapp/models/profile.dart';
+import 'package:flutter_ecoapp/models/question.dart';
 import 'package:flutter_ecoapp/models/user.dart';
 import 'package:flutter_ecoapp/providers/article_api.dart';
 import 'package:flutter_ecoapp/providers/opinion_api.dart';
+import 'package:flutter_ecoapp/providers/question_api.dart';
 
 class ArticleBloc extends BaseBloc<ArticleModel>{
 
   final articleAPI = ArticleAPI();
   final opinionAPI = OpinionAPI();
+  final questionAPI = QuestionAPI();
 
   ArticleBloc(initialState) : super(initialState);
 
@@ -28,6 +31,22 @@ class ArticleBloc extends BaseBloc<ArticleModel>{
     required OpinionModel opinion,
   }) async => (await opinionAPI.insert(
     item: opinion,
+    additionalParams: {
+      'article': article.id,
+      'profile': profile.id
+    }
+  )) != null;
+
+  Future<List<QuestionModel>> getQuestionsFromArticle(ArticleModel article) async => await questionAPI.selectAll(params: {
+    'article': article.id
+  });
+
+  Future<bool> newQuestionToArticle({
+    required ArticleModel article,
+    required ProfileModel profile,
+    required QuestionModel question
+  }) async => (await questionAPI.insert(
+    item: question,
     additionalParams: {
       'article': article.id,
       'profile': profile.id
