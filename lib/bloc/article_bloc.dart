55b37924c04c3879724f7297bf0287d6b3fa9032
IQuinteros <i.quinteros@hotmail.com>
2021-06-09 15:48:46 -0400

    Adding article opinions to bloc

--- lib/bloc/article_bloc.dart
+++ lib/bloc/article_bloc.dart
@@ -1,11 +1,15 @@
 import 'package:flutter_ecoapp/bloc/base_bloc.dart';
 import 'package:flutter_ecoapp/models/article.dart';
+import 'package:flutter_ecoapp/models/opinion.dart';
+import 'package:flutter_ecoapp/models/profile.dart';
 import 'package:flutter_ecoapp/models/user.dart';
 import 'package:flutter_ecoapp/providers/article_api.dart';
+import 'package:flutter_ecoapp/providers/opinion_api.dart';
 
 class ArticleBloc extends BaseBloc<ArticleModel>{
 
   final articleAPI = ArticleAPI();
+  final opinionAPI = OpinionAPI();
 
   ArticleBloc(initialState) : super(initialState);
 
@@ -13,6 +17,22 @@ class ArticleBloc extends BaseBloc<ArticleModel>{
     'search': search,
     'user': user.id
   });
+
+  Future<List<OpinionModel>> getOpinionsFromArticle(ArticleModel article) async => await opinionAPI.selectAll(params: {
+    'article': article.id
+  });
+
+  Future<bool> newOpinionToArticle({
+    required ArticleModel article, 
+    required ProfileModel profile,
+    required OpinionModel opinion,
+  }) async => (await opinionAPI.insert(
+    item: opinion,
+    additionalParams: {
+      'article': article.id,
+      'profile': profile.id
+    }
+  )) != null;
   
   @override
   Stream mapEventToState(event) {
