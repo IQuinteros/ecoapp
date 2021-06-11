7b090ff0934fb74d2a229a8101d15e75458a5ca7
IQuinteros <i.quinteros@hotmail.com>
2021-06-11 02:32:12 -0400

    Go to home after purchase

--- lib/views/summary_view.dart
+++ lib/views/summary_view.dart
@@ -1,15 +1,16 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_ecoapp/bloc/app_bloc.dart';
 import 'package:flutter_ecoapp/bloc/cart_bloc.dart';
 import 'package:flutter_ecoapp/bloc/profile_bloc.dart';
+import 'package:flutter_ecoapp/models/article.dart';
 import 'package:flutter_ecoapp/models/cart.dart';
-import 'package:flutter_ecoapp/models/purchase.dart';
-import 'package:flutter_ecoapp/models/store.dart';
-import 'package:flutter_ecoapp/views/chat_view.dart';
+import 'package:flutter_ecoapp/utils/currency_util.dart';
 import 'package:flutter_ecoapp/views/style/colors.dart';
-import 'package:flutter_ecoapp/views/style/text_style.dart';
 import 'package:flutter_ecoapp/views/widgets/articles/article_card.dart';
-import 'package:flutter_ecoapp/views/widgets/bottom_nav_bar.dart';
+import 'package:flutter_ecoapp/views/widgets/articles/mini_eco_indicator.dart';
+import 'package:flutter_ecoapp/views/widgets/normal_button.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SummaryView extends StatelessWidget {
@@ -45,6 +46,10 @@ class SummaryView extends StatelessWidget {
   Widget getContent(BuildContext context){
     final cartBloc = BlocProvider.of<CartBloc>(context);
     final loadedArticles = cartBloc.loadedArticles;
+
+    CartModel cart = CartModel(
+      articles: loadedArticles.map<ArticleModel>((e) => e.article!).toList()
+    );
 
     final profileBloc = BlocProvider.of<ProfileBloc>(context);
 
@@ -124,6 +129,81 @@ class SummaryView extends StatelessWidget {
       ],
     );
 
+    final purchaseData = Column(
+      children: [
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(width: 20.0),
+            Expanded(
+              child: Text(
+                'Total: ${CurrencyUtil.formatToCurrencyString(cart.totalPrice.toInt(), symbol: '\$')}',
+                style: GoogleFonts.montserrat(
+                  fontSize: 18
+                ),
+                textAlign: TextAlign.start,
+              ),
+            ),
+            SizedBox(width: 20.0),
+          ],
+        ),
+        SizedBox(height: 10),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(width: 20.0),
+            Expanded(
+              child: Text(
+                'Fecha de compra: ${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}',
+                style: GoogleFonts.montserrat(
+                ),
+                textAlign: TextAlign.start,
+              ),
+            ),
+            SizedBox(width: 20.0),
+          ],
+        ),
+        SizedBox(height: 10),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(width: 20.0),
+            Expanded(
+              child: Text(
+                'Dirección: ${profileBloc.currentProfile!.location}',
+                style: GoogleFonts.montserrat(
+                ),
+                textAlign: TextAlign.start,
+              ),
+            ),
+            SizedBox(width: 20.0),
+          ],
+        ),
+        SizedBox(height: 10),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(width: 20.0),
+            Expanded(
+              child: Text(
+                'Sellos amigables: ',
+                style: GoogleFonts.montserrat(
+                ),
+                textAlign: TextAlign.start,
+              ),
+            ),
+            MiniEcoIndicator(ecoIndicator: cart.summaryEcoIndicator),
+            SizedBox(width: 20.0),
+          ],
+        ),
+        SizedBox(height: 10),
+      ],
+    );
+
     final content = Column(
       children: [
         Divider(thickness: 1,),
@@ -154,6 +234,41 @@ class SummaryView extends StatelessWidget {
         SizedBox(height: 10.0),
         profileData,
         Divider(thickness: 1),
+        SizedBox(height: 10.0),
+        purchaseData,
+        Divider(thickness: 1),
+        SizedBox(height: 10.0),
+        Container(
+          child: NormalButton(
+            text: 'Procesar pedido', 
+            onPressed: (){
+              final appBloc = BlocProvider.of<AppBloc>(context);  
+
+              // TODO: API Purchase
+              AwesomeDialog(
+                title: 'Pedido exitoso',
+                desc: 'Has realizado tu pedido exitósamente :)',
+                dialogType: DialogType.SUCCES, 
+                animType: AnimType.BOTTOMSLIDE,
+                context: context,
+                btnOkText: 'Aceptar',
+                btnOkOnPress: () {
+                  Navigator.popUntil(context, ModalRoute.withName('/'));
+                  appBloc.mainEcoNavBar.onTap(0);
+                },
+                onDissmissCallback: (__) {
+                  Navigator.popUntil(context, ModalRoute.withName('/'));
+                  appBloc.mainEcoNavBar.onTap(0);
+                },
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0)
+              )..show();
+              // TODO: Clean cart
+            },
+          ),
+          margin: EdgeInsets.symmetric(
+            horizontal: 20.0
+          ),
+        )
       ],
     );
 
