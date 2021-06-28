5ac69fd39d4b71c6bead932f2a812d7eb850650b
IQuinteros <i.quinteros@hotmail.com>
2021-06-27 20:56:57 -0400

    Fixes, splash, icon and meta info

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecoapp/views/style/colors.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  
+  AlignmentGeometry alignment = Alignment.center;
+
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(milliseconds: 1000), () => setState(() => alignment = Alignment.bottomCenter));
     return Scaffold(
-      body: Center(
-        child: Icon(
-          Icons.apartment_outlined,
-          size: MediaQuery.of(context).size.width * 0.785,
+      backgroundColor: EcoAppColors.BLACK_COLOR,
+      body: AnimatedAlign(
+        alignment: alignment,
+        curve: Curves.easeInCirc,
+        duration: Duration(milliseconds: 1500),
+        child: Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: 20.0,
+            vertical: 20.0
+          ),
+          child: Image(
+            image: AssetImage('assets/png/logo.png'),
+          ),
         ),
       ),
     );
